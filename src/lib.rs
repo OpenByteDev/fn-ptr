170985d1c2adc3c1c8e402e797c4239df391ef92
OpenByte <development.openbyte@gmail.com>
2025-11-26 01:34:30 +0100

    Fix ci

--- src/lib.rs
+++ src/lib.rs
@@ -106,7 +106,7 @@
 //!
 //! ## License
 //!
-//! Licensed under the MIT license, see [LICENSE](./LICENSE) for details.
+//! Licensed under the MIT license, see [LICENSE](../LICENSE) for details.
 
 use core::{
     fmt::{Debug, Pointer},
